d7fdb062376ca031f5d89df73b2d169b4700790f
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-04 22:29:53 +0000

    Prime generation logic
    The meat of the problem is here. I have based this first version on the sieve of Eratostenes, but due that we request a number of primes, and we don't know how many test are required to get that number, the order of operations has to change on regards to the standard algorithm. Stream is used to provide this lazy, infinite loop. Due to how most functional languages work (lack of state, parameters are the way to control results), the different subparts have been tried on iex, before being added into the file all together.

--- lib/primes_generator.ex
+++ lib/primes_generator.ex
@@ -1,8 +1,24 @@
 defmodule PrimesGenerator do
   def generate_prime(number_of_primes) do
-	case number_of_primes do
-	  1 -> [2]
-	  2 -> [2, 3]
-	end
+	Enum.take(Stream.unfold([],
+		  fn(generated) ->
+			next_prime = select_next_prime(generated)
+			{next_prime, [next_prime | generated]}
+		  end),
+	  number_of_primes)
+  end
+
+  defp select_next_prime([]), do: 2
+  defp select_next_prime(generated) do
+	next = List.first(generated) + 1
+	Stream.iterate(next, &(&1+1))
+	|> Stream.drop_while(fn(candidate) ->
+	  Enum.any?(generated, fn(x) ->
+		rem(candidate, x) == 0
+	  end)
+	end)
+	|> Stream.take(1)
+	|> Enum.to_list
+	|> List.first
   end
 end
