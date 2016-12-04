8c4ff6807a79cdb04c6a009db13ea2cfc8171723
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-04 22:38:53 +0000

    Refactoring sieve
    I have refactored the important part of the sieve (checking against previous primes) into a separate function. That way, the select_next_prime is much easier to read. I have changed as well select_next_prime([]) to follow the guidelines previously mentioned

--- lib/primes_generator.ex
+++ lib/primes_generator.ex
@@ -8,17 +8,25 @@ defmodule PrimesGenerator do
 	  number_of_primes)
   end
 
-  defp select_next_prime([]), do: 2
+  defp select_next_prime([]) do
+	2
+  end
+  
   defp select_next_prime(generated) do
 	next = List.first(generated) + 1
 	Stream.iterate(next, &(&1+1))
-	|> Stream.drop_while(fn(candidate) ->
+	|> sieve(generated)
+	|> Stream.take(1)
+	|> Enum.to_list
+	|> List.first
+  end
+
+  defp sieve(numbers, generated) do
+	Stream.drop_while(numbers,
+	  fn(candidate) ->
 	  Enum.any?(generated, fn(x) ->
 		rem(candidate, x) == 0
 	  end)
 	end)
-	|> Stream.take(1)
-	|> Enum.to_list
-	|> List.first
   end
 end
