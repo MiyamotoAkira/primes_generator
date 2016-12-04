e0de307cedc2f3747ff59607c7650d93b258a33b
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-04 22:16:16 +0000

    Added new tests(not passing)
    These two new tests will help in creating a generic solution for the generation of primes. The first one checks that we receive as many primes as requested, the second just makes a check that all primes are not divisible by 2.

--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule PrimesGenerator.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+	  {:ex_parameterized, "~> 1.1.0"}
+	]
   end
 end
--- test/primes_generator_test.exs
+++ test/primes_generator_test.exs
@@ -1,5 +1,6 @@
 defmodule PrimesGeneratorTest do
   use ExUnit.Case
+  use ExUnit.Parameterized
   doctest PrimesGenerator
   import PrimesGenerator
 
@@ -9,5 +10,31 @@ defmodule PrimesGeneratorTest do
 
   test "When asking for two primes it will return 2 and 3" do
     assert [2, 3] == PrimesGenerator.generate_prime 2
+  end
+
+  test_with_params "When asking for a number of primes, that amount should be returned",
+  fn(number_of_primes) ->
+	assert Enum.count(PrimesGenerator.generate_prime(number_of_primes)) == number_of_primes
+  end do
+	[
+	  {1},
+	  {4},
+	  {8},
+	  {16}
+	]
+  end
+
+  test_with_params "When asking for a number of primes, only the first one should be divisible by 2",
+  fn(number_of_primes) ->
+	assert (PrimesGenerator.generate_prime(number_of_primes)
+	  |> Enum.drop(1)
+	  |> Enum.all?(fn(x) -> rem(x,2) ==1 end))
+  end do
+	[
+	  {1},
+	  {4},
+	  {8},
+	  {16}
+	]
   end
 end
