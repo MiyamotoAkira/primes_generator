abc8b884bbf8463fa49449db782ee1cd48dbffeb
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-05 16:04:29 +0000

    Performance improvement
    An easy win, I have converted the straight multiplication into a async/await tasks. Non-very scientific findings means a 5x improvement on the 1000 test, and 10x on the 10000.

--- lib/multiplication.ex
+++ lib/multiplication.ex
@@ -1,5 +1,7 @@
 defmodule Multiplication do
   def cross_multiplication(numbers) do
-	Enum.map(numbers, fn(x) -> Enum.map(numbers, fn(y) -> x * y end) end)
+	numbers
+	|> Stream.map(&Task.async(fn -> Enum.map(numbers, fn(y) -> &1 * y end)end))
+	|> Enum.map(&Task.await(&1))  
   end
 end
