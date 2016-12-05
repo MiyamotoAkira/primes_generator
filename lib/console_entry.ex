56afcafe60f5f564c868e905ee2f4e3c6c2961f1
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-05 00:29:48 +0000

    Changes on the generation of the invalid message
    A couple of changes, related to the message and how the formatted value is acquire have been done to clear a bit the system

--- lib/console_entry.ex
+++ lib/console_entry.ex
@@ -23,15 +23,16 @@ defmodule ConsoleEntry do
   end
 
   def process(converted) do
-	error_message = "You have passed the following incorrect values"
+	error_message = "You have passed the following incorrect values:"
 	command_suggestion = "The following is the expected input:"
-	command_option = "console_entry <number_of_primes> [--help|-h]"
+	command_option = "console_entry <number_of_primes>|[--help|-h]"
 	case converted do
 	  {:invalid, value} ->
+		formatted =
 		if is_list(value) do
-		  formatted = Enum.join(value, " ")
+		  Enum.join(value, " ")
 		else
-		  formatted = value
+		  value
 		end
 		
 		"#{error_message} #{formatted}\n#{command_suggestion}\n#{command_option}"
