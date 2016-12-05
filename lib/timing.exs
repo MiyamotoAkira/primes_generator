2bb49b6a490a30fae6164422b4ccc57815191e86
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-05 14:20:07 +0000

    Basic benchmarking
    A very basic benchmarking has been added, to try to improve the application

---
+++ lib/timing.exs
@@ -0,0 +1,15 @@
+defmodule Timing do
+  
+  def run_for_1000 do
+	result = PrimesGenerator.generate_prime(1000)
+	{time, _} =  :timer.tc(Multiplication, :cross_multiplication, [result])
+	IO.puts time
+  end
+
+  def run_for_10000 do
+	result = PrimesGenerator.generate_prime(10000)
+	{time, _} =  :timer.tc(Multiplication, :cross_multiplication, [result])
+	IO.puts time
+  end
+
+end
