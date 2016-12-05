8d5be1339e939b171f5d89166d54a638712ee92e
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-05 07:33:56 +0000

    Refactored messages
    The creation of messages has been moved to separate methods, and the strings have been converted into constants/attributes

--- lib/console_entry.ex
+++ lib/console_entry.ex
@@ -1,6 +1,14 @@
 defmodule ConsoleEntry do
   import PrimesGenerator
 
+  @error_message "You have passed the following incorrect values:"
+  @command_suggestion "The following is the expected input:"
+  @command_option "console_entry <number_of_primes>|[--help|-h]"
+  @first_line "console_entry will help you on finding the multiples of a selection of primes."
+  @second_line "The only parameter needed is the number of primes that you want to use"
+  @example_string "The following is an example"
+  @example "console_entry 3"
+
   def main(args) do
 	args
 	|> parse_args
@@ -23,26 +31,25 @@ defmodule ConsoleEntry do
   end
 
   def process(converted) do
-	error_message = "You have passed the following incorrect values:"
-	command_suggestion = "The following is the expected input:"
-	command_option = "console_entry <number_of_primes>|[--help|-h]"
 	case converted do
-	  {:invalid, value} ->
-		formatted =
+	  {:invalid, value} -> craft_invalid_message(value)
+		
+	  :help -> craft_help_message()
+	end
+  end
+
+  defp craft_invalid_message(value) do
+	formatted =
 		if is_list(value) do
 		  Enum.join(value, " ")
 		else
 		  value
 		end
 		
-		"#{error_message} #{formatted}\n#{command_suggestion}\n#{command_option}"
-	  :help ->
-		first_line = "console_entry will help you on finding the multiples of a selection of primes."
-		second_line = "The only parameter needed is the number of primes that you want to use"
-		example_string = "The following is an example"
-		example = "console_entry 3"
+	"#{@error_message} #{formatted}\n#{@command_suggestion}\n#{@command_option}"
+  end
 
-		"#{first_line}\n#{second_line}\n#{command_option}\n\n#{example_string}\n#{example}"
-	end
+  defp craft_help_message() do
+	"#{@first_line}\n#{@second_line}\n#{@command_option}\n\n#{@example_string}\n#{@example}"
   end
 end
