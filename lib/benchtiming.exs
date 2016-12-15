6f0403dfde5930d70c7006a3f34ed5edeb8e12c7
MiyamotoAkira <yefo.akira@gmail.com>
2016-12-15 07:26:45 +0000

    Starting with documentation
    Started adding some documentation to the system. Right now some basic example so I can repliacte

--- lib/benchtiming.exs
+++ lib/benchtiming.exs
@@ -1,5 +1,18 @@
 defmodule BenchTiming do
+  @moduledoc """
+  This is the benchmarking module.
 
+  Quite simple, it uses the Erlang Timer library. At the moment it captures both main operations on a single method call and outputs im the console. Need improvement
+  """
+
+  @doc """
+  Executes the code using Trial Division until it gets `number` of prime numbers
+
+  ## Example
+
+    iex> BenchTiming.run_trial(5)
+    :ok
+  """
   def run_trial(number) do
 	run(number, &PrimesGenerator.Sieves.TrialDivision.generate_prime/1)
   end
@@ -8,15 +21,7 @@ defmodule BenchTiming do
 	run(number, &PrimesGenerator.Sieves.TrialDivisionImp.generate_prime/1)
   end
     
-  def run_for_1000 do
-	run(1000, &PrimesGenerator.Sieves.TrialDivision.generate_prime/1)
-  end
-
-  def run_for_10000 do
-	run(10000, &PrimesGenerator.Sieves.TrialDivision.generate_prime/1)
-  end
-
-  def run(number, sieve_function) do
+  defp run(number, sieve_function) do
     {time, result} = :timer.tc(sieve_function, [number])
 	IO.puts time
 	{time, _} =  :timer.tc(Multiplication, :cross_multiplication, [result])
